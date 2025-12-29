0e54bdc196aff6b5022b8be4f1d3c93162045650
syedabaan4 <syed.abaan4444@gmail.com>
2025-12-29 05:57:34 +0500

    report done

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
-from stats import count_words, get_book_text
+from stats import *
 
 def main():
     path = "./books/frankenstein.txt"
-    # print(count_words(get_book_text(path)))
     num = count_words(get_book_text(path))
+    print("============ BOOKBOT =============")
+    print(f"analyzing book found at {path}")
+    print("--------- Word Count ----------")
     print(f"Found {num} total words")
+    print("------- Character Count -------")
+
+    char_count = count_characters(get_book_text(path))    
+    # print(char_count)
+    final_list =  sort_dict(char_count)
+    for item in final_list:
+        char = item["char"]
+        number = item["num"]
+        print(f"{char}: {number}")
+
+    print("============ END =============")
 
 main()
--- stats.py
+++ stats.py
@@ -1,3 +1,5 @@
+import string
+
 def get_book_text(filepath):
     with open(filepath) as f:
         file_contents = f.read()
@@ -7,4 +9,34 @@ def count_words(text):
     word_list = text.split()
     count = len(word_list) 
     return count
-    
+
+def count_characters(text):
+    stripped = text.strip(" ")
+    lower_case = stripped.lower()
+
+    # create an array of all characters
+    alphabet_list = list(string.ascii_lowercase)
+        
+    # create empty dict
+    counts = {}
+
+    # for each char in array (key) count occurences (value)
+    for i in alphabet_list:
+        counts[i] = lower_case.count(f"{i}")
+
+    return counts
+
+def sort_on(items):
+    return items["num"]
+
+def sort_dict(dictionary):
+    dict_list = []
+
+    for key, value in dictionary.items():
+        new_dict = {}
+        new_dict["char"] = key
+        new_dict["num"] = value
+        dict_list.append(new_dict)
+
+    dict_list.sort(reverse=True, key=sort_on)
+    return dict_list
